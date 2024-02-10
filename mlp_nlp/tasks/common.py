4eb3323c4b3acdacb508b6c5e407bfdc4a89fdfe
dimits <tsirbasdim@gmail.com>
2024-02-10 12:23:14 +0200

    Add final training stage

--- mlp_nlp/tasks/common.py
+++ mlp_nlp/tasks/common.py
@@ -1,5 +1,7 @@
 import os
 import matplotlib.pyplot as plt
+from tensorflow import keras
+import keras.backend as K
 
 
 def save_plot(filename: str, dir_name: str = "output") -> None:
@@ -16,3 +18,18 @@ def save_plot(filename: str, dir_name: str = "output") -> None:
     
     plt.savefig(path, bbox_inches="tight")
     print(f"Figured saved to " + path)
+
+
+def reset_weights_keras(model: keras.Model) -> keras.Model:
+    """
+    Reset the weights of any keras model.
+
+    :param model: The model to be reset.
+    :return: the same model with random weights
+    """
+    session = K.get_session()
+    for layer in model.layers: 
+        if hasattr(layer, 'kernel_initializer'): 
+            layer.kernel.initializer.run(session=session)
+        if hasattr(layer, 'bias_initializer'):
+            layer.bias.initializer.run(session=session)  
