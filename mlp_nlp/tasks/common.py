2fb52e927cf95d216a22f860261e4cbb6d124d62
dimits <tsirbasdim@gmail.com>
2024-02-10 11:05:19 +0200

    Add save plot functionality

---
+++ mlp_nlp/tasks/common.py
@@ -0,0 +1,18 @@
+import os
+import matplotlib.pyplot as plt
+
+
+def save_plot(filename: str, dir_name: str = "output") -> None:
+    """
+    Saves a plot to the output directory.
+
+    :param filename: The name of the file for the Figure.
+    :type filename: str
+    """
+    path = os.path.join(dir_name, filename)
+    
+    if not os.path.exists(dir_name): 
+        os.makedirs(dir_name) 
+    
+    plt.savefig(path, bbox_inches="tight")
+    print(f"Figured saved to " + path)
